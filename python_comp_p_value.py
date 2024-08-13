a257c69094afedf50b4baaf5c79542a6c70f8df5
Sherry Gao <qgao@conncoll.edu>
2024-08-13 19:16:46 -0400

    Add files via upload

---
+++ python_comp_p_value.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy import stats
+
+# KMT2D blood glucose levels
+kmt2d_glucose = np.array([0.78, 1.2, 0.6, 1.7, 0.7, 1.0, 1.33, 0.85, 1.4, 1.5, 0.95, 1.25])
+
+# KDM6A blood glucose levels
+kdm6a_glucose = np.array([0.87, 0.66, 0.92, 1.21, 0.63, 0.63, 1.23, 0.96, 0.55, 0.88, 0.55, 0.55])
+
+# Perform an independent t-test assuming equal variances
+t_stat, p_value_equal_var = stats.ttest_ind(kmt2d_glucose, kdm6a_glucose, equal_var=True)
+
+
+print("t-statistic:", t_stat)
+print("p-value:", p_value)
